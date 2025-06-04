6b7d5f3fb8c9b6b91d6d52be1e9275c89ec834c8
Paulo Vieira <paulohenriqueavieira@gmail.com>
2025-06-04 18:16:23 -0300

    Limitando o envio do formulário apenas quando todas as perguntas estiverem respondidas

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,8 +1,21 @@
 let pontos = 0;
 let botao = document.querySelector('#Btnenviar')
 
+
+
 botao.addEventListener('click', function(event){
     event.preventDefault();
+
+    for(let i = 1; i<10;i++){
+        if(document.querySelector(`input[name="pergunta${i}"]:checked`) == null){
+            alert("Responda todas as questões antes de enviar as respostas.")
+            return
+        }
+    }
+
+
+
+
     if(document.querySelector('input[name="pergunta1"]:checked').value == '1'){
         pontos++
     }
@@ -49,3 +62,25 @@ botao.addEventListener('click', function(event){
     }
 })
 
+const standart = document.getElementById("standart-mode");
+const dark = document.getElementById("dark-mode");
+const black = document.getElementById("black-mode");
+//---------------------------------- criando a função
+function applytheme(themeClass){
+    document.body.classList.remove("darkcolors", "blackcolors")
+
+    if(themeClass !== "standart"){
+        document.body.classList.add(themeClass)
+    }
+}
+
+//----------------------------------chamando a função
+standart.addEventListener("click", () => {
+    applytheme("standart"); 
+});
+dark.addEventListener("click", () => {
+    applytheme("darkcolors"); 
+});
+black.addEventListener("click", () => {
+    applytheme("blackcolors"); 
+});
